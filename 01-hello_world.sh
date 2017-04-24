7ac1907167d4b320d0dcc4ffc791de9f4f014855
Justine Boulanger <bouljujube@gmail.com>
2017-04-24 17:33:59 -0400

    Ajustement Fichier 01

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+echo "salut"
+#exit
+echo "toi"
 
+# -n prévenir le retour de ligne automatiue
+echo -n "Vive"
+echo " Linux"
+
+# -e  accepter les métas-caractères
+echo -e "1\n2\n3"
+
+#$ = Lorsque l'on veut la valeur de la variable (lecture ou valeur locale)
+# sinon, on affecte dans la variable (sans le $)
+
+read -p "Quel âge avez-vous?" age
+echo $age "ans! Vous paraissez jeune!"

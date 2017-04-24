15ad116b9c976112756b9814d4f7c7e432a84798
Justine Boulanger <bouljujube@gmail.com>
2017-04-24 17:41:08 -0400

    Variable.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+#Ne pas mettre d'espaces!!
+prenom=John
+echo $prenom
+declare -i age=35	 # un int
+declare -r age2=35 	#Devient en lecture seule
+
+#On met des guillements lorsq'il y a des espaces dans les noms
+cours="Cours de linux"
+
+echo Le programme $0 a pris le paramtètre $1

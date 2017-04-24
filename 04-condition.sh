b35aaea2593bf42aa9e3077ccfcc8fb60a48279e
Justine Boulanger <bouljujube@gmail.com>
2017-04-24 17:54:51 -0400

    04 conditions (if)

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then 
+	echo "Give up"
+elif test $note -eq 60
+then 
+	echo "Safe!"
+else
+	echo "Nice"
+fi
 
+read -p "Entrez une lettre:" lettre
 
+case $lettre in
+	[[:lower:p]])
+		echo "lettre en minuscule"	
+		;;
+	*)
 
+	echo "Majuscule"
+		;;
+
+esac
+
+if test $lettre != "a"
+then
+	echo "la lettre n'est pas a"
+fi
 
 
 

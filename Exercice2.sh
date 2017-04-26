70aab15b6a74570b5009b2edb255425abdb33cb9
Justine Boulanger <bouljujube@gmail.com>
2017-04-26 15:59:37 -0400

    Exercice 2 Programme menu

--- Exercice2.sh
+++ Exercice2.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+maFonction()  {
+	clear
+	echo "Ceci n'est pas un menu"
+	echo "Choix 1 : Démarrer le programme de dictionnaire"
+	echo "Choix 2 : Continuer la boucle"
+	echo "Choix q : Quitter"
+}
+reponse="a"
+while test $reponse != "q"
+do
+	maFonction
+	read -p "Entrez une lettre : " reponse
+	if [[ $reponse = 1 ]]
+	then
+		 ./Exercice1.sh
+		 sleep  5
+	else
+	maFonction
+	fi
+done
+
+

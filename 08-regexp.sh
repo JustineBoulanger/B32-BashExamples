2309aa9e39a04ca0556ebc7df7bb8b3490901985
Justine Boulanger <bouljujube@gmail.com>
2017-04-26 15:11:29 -0400

    08 Expressions régulières

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+echo -n "Lettre: "
+read lettre
+
+# peut être remplacé par read -p "Lettre :" lettre
+
+#[[]] = test
+# ~ =  expression régulière (regexp)
+#Cette forme du if vérifie que ce qui est entré n'est que des nombres
+
+if  [[ $lettre =~ ^[0-9]+$ ]]
+then 
+	echo "C'est un nombre"
+fi
+
+
+if  [[ $lettre =~ ^[a-zA-Z]+$ ]]
+then
+        echo "C'est un mot"
+fi
+
+

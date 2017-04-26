01a0e398039d7f424bb22b8cad4627cf13294a0e
Justine Boulanger <bouljujube@gmail.com>
2017-04-26 15:02:49 -0400

    Exercice 07 Fonction

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+maFonction() {
+	echo "Salut"
+}
+
+maFonction
+maFonction
+maFonction
+
+maFonction() {
+        echo "Salut $1"
+}
+
+maFonction MarcO
+maFonction Jambon
+maFonction xml
+

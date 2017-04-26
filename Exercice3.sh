bcc47e4630f7a4fb2b828d64bc5561f0befeb70d
Justine Boulanger <bouljujube@gmail.com>
2017-04-26 16:46:40 -0400

    Exercice 3

--- Exercice3.sh
+++ Exercice3.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+Espace() {
+	nb=$1
+	i=0
+	while test $i -lt $nb
+	do
+		i=$(($i+1))
+		echo -n " "
+	done
+}
+for ((i=0;i<30;i++))
+do
+	clear
+	Espace $i
+	echo "@"
+	sleep 0.1
+done
+
+clear
+Espace 30
+echo "BOOM!!!"
+
